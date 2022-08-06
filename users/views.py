4a234b2caa3a3749d960b4cdcd2f9fe9dfc410b7
Martin Mendez <martinpercu@gmail.com>
2022-08-06 02:40:04 +0200

    Logout logic + logout html + block logout if not logged before

--- users/views.py
+++ users/views.py
@@ -2,13 +2,13 @@
 
 #Django
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required  #nor really needed because is not possible to arrive the view if you are not already logged
 # Create your views here.
 
 
 def login_view(request):
-    """Login view"""
+    """ Login view """
 
     # import pdb; pdb.set_trace()
 
@@ -22,5 +22,13 @@ def login_view(request):
             login(request, user)
             return redirect('feed')
         else:
-            return render(request, 'users/login.html', {'error': 'Invalid username or password'})
+            return render(request, 'users/login.html', {'error': 'The username and/or password not found'})
     return render(request, 'users/login.html')
+
+@login_required #in normal use no need this decorator.---> You are in this 'post' page if you are already logged.
+def logout_view(request):
+    """ Logout view """
+
+    logout(request)
+
+    return render(request, 'users/login.html', {'messageLogout': 'You just logout.'})
