0c19c80ec9d48e8bcbeff1a154b2993a5a2944dc
Martin Mendez <martinpercu@gmail.com>
2022-08-06 16:20:05 +0200

    Register user OK + HTML + Redirects if user already exists OK

--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
 """ This is the users view"""
 
-#Django
+# Django
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required  #nor really needed because is not possible to arrive the view if you are not already logged
+
+# Exception
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from .models import Profile
 # Create your views here.
 
 
@@ -32,3 +39,32 @@ def logout_view(request):
     logout(request)
 
     return render(request, 'users/login.html', {'messageLogout': 'You just logout.'})
+
+
+def signup(request):
+    """Signup View"""
+    # import pdb; pdb.set_trace()
+    print(request.POST)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'password_error': 'Both password must be the same'})
+        
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already used'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
