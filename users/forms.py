b5889790ef808be17b2cad222ffe38147e4ecd8f
Martin Mendez <martinpercu@gmail.com>
2022-08-15 15:29:01 +0200

    validation django form in the html for the user

--- users/forms.py
+++ users/forms.py
@@ -9,6 +9,6 @@ class ProfileForm(forms.Form):
 
     website = forms.URLField(max_length=200, required=True)
     biography = forms.CharField(max_length=500, required=False)
-    phone_number = forms.CharField(max_length=18, required=False)
+    phone_number = forms.CharField(max_length=14, required=False)
     picture = forms.ImageField()
 
