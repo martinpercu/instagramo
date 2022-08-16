4499f14a4cc789f129ec1a870361b6e482c221fa
Martin Mendez <martinpercu@gmail.com>
2022-08-16 18:59:14 +0200

    update_user using Forms by Django with validation --- check the password validation not working if to simple in DB

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,65 @@
 
 # Django
 from django import forms
+from django.core.exceptions import ValidationError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+
+
+class SignupForm(forms.Form):
+    """ Sirg up form """
+
+    username = forms.CharField(min_length=4, max_length=48)
+    
+    password = forms.CharField(
+        max_length=72, 
+        widget=forms.PasswordInput()
+    )
+    password_confirmation = forms.CharField(
+        max_length=72, 
+        widget=forms.PasswordInput()
+    )
+
+    first_name = forms.CharField(min_length=2, max_length=48)
+    last_name = forms.CharField(min_length=2, max_length=48)
+
+    email = forms.CharField(
+        min_length=6,
+        max_length=90,
+        widget=forms.EmailInput()
+    )
+
+    def clean_username(self):
+        """ username must to be unique """
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        if username_taken:
+            raise ValidationError("User name is already used for another one")
+        return username
+
+    def clean(self):
+        """ Verify password and passwords confirmation MATCH"""
+        data = super().clean()
+
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+
+        if password != password_confirmation:
+            raise ValidationError('Passwords are not the same!')
+
+        return data
+    
+    def save(self):
+        """ Create user and Profile --- Is both at same time """
+        data = self.cleaned_data
+        data.pop('password_confirmation')
+
+        user = User.objects.create(**data)
+        profile = Profile(user=user)
+        profile.save()
 
 
 class ProfileForm(forms.Form):
--- users/views-old.py
+++ users/views-old.py
@@ -5,11 +5,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required  #nor really needed because is not possible to arrive the view if you are not already logged
 
+# Exception
+from django.db.utils import IntegrityError
 
+# Models
+from django.contrib.auth.models import User
+from .models import Profile
 # Create your views here.
 
 # Forms
-from users.forms import ProfileForm, SignupForm
+from users.forms import ProfileForm
 
 # def for an experimental django Middleware.... 
 
@@ -78,16 +83,28 @@ def logout_view(request):
 
 def signup(request):
     """Signup View"""
+    # import pdb; pdb.set_trace()
+    print(request.POST)
     if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = SignupForm()
-    return render(
-        request=request,
-        template_name='users/signup.html',
-        context={'form': form}
-    )
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
         
+        if password != password_confirmation:
+            return render(request, 'users/signup.html', {'password_error': 'Both password must be the same'})
+        
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already used'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
